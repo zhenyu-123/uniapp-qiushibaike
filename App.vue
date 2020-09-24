17d485b14e4b39ac05e264f578b527779f79d644
isSy <15110320048>
2020-09-24 19:04:38 +0800

    'one'

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 官方css库 */
+	@import url("./common/uni.css");
+	
+	
+</style>
