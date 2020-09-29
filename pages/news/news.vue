94a8384f192eaea2a299db1fa67dbf2c798d032e
Your Name <you@example.com>
2020-09-29 22:33:05 +0800

    'add'

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -1,15 +1,35 @@
 <template>
 	<view>
-		news
+		<uniNavBar :border="false" :fixed="true" :statusBar="true">
+			<view class="flex align-center justify-center font-weight-bold w-100">
+				<view class="mx-1 font-md"  :class="tabindex===index?'font-lg text-main':'text-light-muted ' "  v-for="(item,index) in tablist" :key="index" @tap="changeTab(index)">{{item.name}}</view>
+			</view>
+			<text @tap="clickright" slot="right" class="iconfont icon-bi"></text>
+		</uniNavBar>
 	</view>
 </template>
 
 <script>
+	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue"
 	export default {
 		data() {
 			return {
-				
+				tabindex:0,
+				tablist:[{name:"关注"},{name:"话题"}]
 			};
+		},
+		methods:{
+			clickright(){
+				uni.navigateTo({
+					url:"../../components/add-input/add-input"
+				})
+			},
+			changeTab(index){
+				this.tabindex=index;
+			}
+		},
+		components:{
+			uniNavBar
 		}
 	}
 </script>
